13cb13906600a3459709fadeeec87d4648e06b5c
hirokaissh <hirokai.saito1014@gmail.com>
2021-10-04 23:34:35 +0900

    tableViewの実装

--- IbarakiUiniversityApp/Controller/DocumentsListViewController.swift
+++ IbarakiUiniversityApp/Controller/DocumentsListViewController.swift
@@ -7,23 +7,33 @@
 
 import UIKit
 
-class DocumentsListViewController: UIViewController {
-
+class DocumentsListViewController: UIViewController{
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let DocumentData = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension DocumentsListViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        return UITableViewCell()
     }
-    */
+}
 
+extension DocumentsListViewController: UITableViewDelegate{
+    
 }
