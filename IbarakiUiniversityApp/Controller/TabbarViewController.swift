38f2a1ff2303220cd1e5a23da796fa172198ea7d
hirokaissh <hirokai.saito1014@gmail.com>
2022-03-20 10:49:51 +0900

    [remove] 不要なコードの削除

--- IbarakiUiniversityApp/Controller/TabbarViewController.swift
+++ IbarakiUiniversityApp/Controller/TabbarViewController.swift
@@ -13,51 +13,6 @@ class TabbarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         self.selectedIndex = 0
-        // error回避
-        if UserDefaults.standard.array(forKey: "SubmitDocuments") == nil {
-            UserDefaults.standard.setValue([], forKey: "SubmitDocuments")
-        }
-
-        if UserDefaults.standard.array(forKey: "LabToDo") == nil {
-            UserDefaults.standard.setValue([], forKey: "LabToDo")
-        }
-        documentitems = UserDefaults.standard.array(forKey: "SubmitDocuments") as? [String] ?? [""]
-        toDoitems = UserDefaults.standard.array(forKey: "LabToDo") as? [String] ?? [""]
-
-        let documenttab: UITabBarItem = self.tabBar.items![0]
-        let toDotab: UITabBarItem = self.tabBar.items![1]
-
-        if documentitems.count == 0 {
-            documenttab.badgeValue = nil
-        } else {
-            documenttab.badgeValue = String(documentitems.count)
-        }
-
-        if toDoitems.count == 0 {
-            toDotab.badgeValue = nil
-        } else {
-            toDotab.badgeValue = String(toDoitems.count)
-        }
-    }
-    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
-        documentitems = UserDefaults.standard.array(forKey: "SubmitDocuments") as? [String] ?? [""]
-        toDoitems = UserDefaults.standard.array(forKey: "LabToDo") as? [String] ?? [""]
-
-        let documenttab: UITabBarItem = self.tabBar.items![0]
-        let toDotab: UITabBarItem = self.tabBar.items![1]
-
-        if documentitems.count == 0 {
-            documenttab.badgeValue = nil
-        } else {
-            documenttab.badgeValue = String(documentitems.count)
-        }
-
-        if toDoitems.count == 0 {
-            toDotab.badgeValue = nil
-        } else {
-            toDotab.badgeValue = String(toDoitems.count)
-        }
     }
 }
