71d31e13f4c88e2df02dd615e96411571afead51
hirokaissh <hirokai.saito1014@gmail.com>
2022-03-22 20:21:46 +0900

    Realmを使用したコードに書き換え

--- IbarakiUiniversityApp/Controller/DocumentsListViewController.swift
+++ IbarakiUiniversityApp/Controller/DocumentsListViewController.swift
@@ -17,7 +17,7 @@ class DocumentsListViewController: UIViewController {
         super.viewDidLoad()
 
         tableView.delegate = self
-        tableView.dataSource = self
+//        tableView.dataSource = self
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -27,53 +27,53 @@ class DocumentsListViewController: UIViewController {
     }
 }
 
-extension DocumentsListViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        documentItems = documentmodel.readData()
-        if documentItems.isEmpty != true {
-            return documentItems.count
-        } else {
-            return 1
-        }
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let documentcell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-
-        documentItems = documentmodel.readData()
-        if documentItems.isEmpty != true {
-            documentcell.textLabel!.text = documentItems[indexPath.row]
-            documentcell.textLabel?.textColor = .black
-            documentcell.textLabel?.backgroundColor = .white
-            return documentcell
-        } else {
-            documentcell.textLabel?.text = "予定されている提出物がありません"
-            documentcell.textLabel?.textColor = .white
-            documentcell.textLabel?.backgroundColor = .darkGray
-            return documentcell
-        }
-    }
-
-    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-
-    func tableView(_ tableView: UITableView,
-                   commit editingStyle: UITableViewCell.EditingStyle,
-                   forRowAt indexPath: IndexPath)
-    {
-        if editingStyle == UITableViewCell.EditingStyle.delete {
-            documentItems = documentmodel.readData()
-            if documentItems.isEmpty != true {
-                documentItems.remove(at: indexPath.row)
-                documentmodel.setData(data: documentItems)
-                tableView.reloadData()
-            } else {
-                return
-            }
-        }
-    }
-}
+//extension DocumentsListViewController: UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        documentItems = documentmodel.readData()
+//        if documentItems.isEmpty != true {
+//            return documentItems.count
+//        } else {
+//            return 1
+//        }
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let documentcell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+//
+//        documentItems = documentmodel.readData()
+//        if documentItems.isEmpty != true {
+//            documentcell.textLabel!.text = documentItems[indexPath.row]
+//            documentcell.textLabel?.textColor = .black
+//            documentcell.textLabel?.backgroundColor = .white
+//            return documentcell
+//        } else {
+//            documentcell.textLabel?.text = "予定されている提出物がありません"
+//            documentcell.textLabel?.textColor = .white
+//            documentcell.textLabel?.backgroundColor = .darkGray
+//            return documentcell
+//        }
+//    }
+//
+//    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+//        return true
+//    }
+//
+//    func tableView(_ tableView: UITableView,
+//                   commit editingStyle: UITableViewCell.EditingStyle,
+//                   forRowAt indexPath: IndexPath)
+//    {
+//        if editingStyle == UITableViewCell.EditingStyle.delete {
+//            documentItems = documentmodel.readData()
+//            if documentItems.isEmpty != true {
+//                documentItems.remove(at: indexPath.row)
+//                documentmodel.setData(data: documentItems)
+//                tableView.reloadData()
+//            } else {
+//                return
+//            }
+//        }
+//    }
+//}
 
 extension DocumentsListViewController: UITableViewDelegate {
     func numberOfSections(in tableView: UITableView) -> Int {
--- IbarakiUiniversityApp/Controller/LabViewController.swift
+++ IbarakiUiniversityApp/Controller/LabViewController.swift
@@ -6,52 +6,66 @@
 //
 
 import UIKit
+import RealmSwift
+
+struct LabToDoCount {
+
+    enum LabToDoType {
+        case non
+        case on
+    }
+
+    func countResult (type: LabToDoType, cell: UITableViewCell) {
+        switch type {
+        case .non:
+            cell.textLabel?.textColor = .black
+            cell.textLabel?.backgroundColor = .white
+        case .on:
+            cell.textLabel?.text = "現在タスクがありません"
+            cell.textLabel?.textColor = .white
+            cell.textLabel?.backgroundColor = .darkGray
+        }
+    }
+}
 
 class LabViewController: UIViewController {
     @IBOutlet private weak var tableView: UITableView!
 
-    var todomodel = ToDoModel()
-    var  toDoItems = [String]()
+//    var todomodel = ToDoModel()
+//    var  toDoItems = [String]()
+    var toDoItems: Results<ToDoModel>!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.dataSource = self
-        tableView.delegate = self
+//        tableView.delegate = self
 
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        print("viewwill")
         tableView.reloadData()
     }
 }
 
 extension LabViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        toDoItems = todomodel.readData()
-        if toDoItems.isEmpty != true {
-            return toDoItems.count
-        } else {
-            return 1
-        }
+        toDoItems.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let toDocell = tableView.dequeueReusableCell(withIdentifier: "ToDoCell", for: indexPath)
-        toDoItems = todomodel.readData()
-
-        if toDoItems.isEmpty != true {
-            toDocell.textLabel?.text = toDoItems[indexPath.row]
-            toDocell.textLabel?.textColor = .black
-            toDocell.textLabel?.backgroundColor = .white
-            return toDocell
+        let toDoCell = tableView.dequeueReusableCell(withIdentifier: "ToDoCell", for: indexPath)
+        let toDoObject = toDoItems[indexPath.row]
+        if toDoItems.count == 0 {
+            toDoCell.textLabel?.text = "現在タスクがありません"
+            toDoCell.textLabel?.textColor = .white
+            toDoCell.textLabel?.backgroundColor = .darkGray
         } else {
-            toDocell.textLabel?.text = "現在タスクがありません"
-            toDocell.textLabel?.textColor = .white
-            toDocell.textLabel?.backgroundColor = .darkGray
-            return toDocell
+            toDoCell.textLabel?.text = toDoObject.labTODO
+            toDoCell.textLabel?.textColor = .black
+            toDoCell.textLabel?.backgroundColor = .white
         }
+        return toDoCell
     }
 
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -69,16 +83,23 @@ extension LabViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView,
                    commit editingStyle: UITableViewCell.EditingStyle,
                    forRowAt indexPath: IndexPath) {
-        if editingStyle == UITableViewCell.EditingStyle.delete {
-            toDoItems = todomodel.readData()
-
-            if toDoItems.isEmpty != true {
-                toDoItems.remove(at: indexPath.row)
-                todomodel.setData(data: toDoItems)
+        if editingStyle == .delete {
+            if toDoItems.count == 0 {
+                deleteLabToDo(at: indexPath.row)
                 tableView.reloadData()
             } else {
                 return
             }
+        }
+    }
+    func deleteLabToDo (at index: Int) {
+        do {
+            let realm = try Realm()
+            try realm.write {
+                realm.delete(toDoItems[index])
+            }
+        } catch {
+            print("Error")
         }
     }
 }
