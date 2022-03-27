519e7fd390761160f46baad371125c00bd3806cb
hirokaissh <hirokai.saito1014@gmail.com>
2022-03-27 16:41:56 +0900

    DocumentCellのXibファイルを作成

---
+++ IbarakiUiniversityApp/View/DocumentTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  DocumentTableViewCell.swift
+//  IbarakiUiniversityApp
+//
+//  Created by HiroakiSaito on 2022/03/27.
+//
+
+import UIKit
+
+class DocumentTableViewCell: UITableViewCell {
+    @IBOutlet weak var documentNameLabel: UILabel!
+    @IBOutlet weak var deadlineLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
