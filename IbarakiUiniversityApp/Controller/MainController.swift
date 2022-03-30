240e914300915b0f40f1fa30af97a54ef02b0f0b
hirokaissh <hirokai.saito1014@gmail.com>
2022-03-30 19:10:57 +0900

    [remove] mainViewControlの削除

--- IbarakiUiniversityApp/Controller/MainController.swift
+++ IbarakiUiniversityApp/Controller/MainController.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 class MainController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
