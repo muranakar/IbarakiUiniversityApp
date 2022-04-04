52a77c5682a3ab88e799785d61b557eb3f3287f8
hirokaissh <hirokai.saito1014@gmail.com>
2022-04-04 21:14:16 +0900

    [remove] 不要な箇所の削除

--- IbarakiUiniversityApp/Controller/TabbarViewController.swift
+++ IbarakiUiniversityApp/Controller/TabbarViewController.swift
@@ -8,9 +8,6 @@
 import UIKit
 
 class TabbarViewController: UITabBarController {
-    private var documentitems = [String]()
-    private var toDoitems = [String]()
-
     override func viewDidLoad() {
         super.viewDidLoad()
         self.selectedIndex = 0
