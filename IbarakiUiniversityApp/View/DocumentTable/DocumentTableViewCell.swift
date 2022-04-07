2486541361385bdd72be6d47f03c6984d160faf9
hirokaissh <hirokai.saito1014@gmail.com>
2022-04-07 23:11:36 +0900

    [upgrade] cellのデザイン変更

--- IbarakiUiniversityApp/View/DocumentTable/DocumentTableViewCell.swift
+++ IbarakiUiniversityApp/View/DocumentTable/DocumentTableViewCell.swift
@@ -10,10 +10,16 @@ import UIKit
 class DocumentTableViewCell: UITableViewCell {
     @IBOutlet private(set) weak var documentNameLabel: UILabel!
     @IBOutlet private(set) weak var deadlineLabel: UILabel!
+    @IBOutlet private(set) weak var documentCell: UIView!
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+
+        
+        documentCell.layer.cornerRadius = 20.0
+        documentCell.layer.shadowOffset = .init(width: 0.5, height: 0.5)
+        documentCell.layer.shadowOpacity = 0.2
+        documentCell.layer.shadowRadius = 20.0
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
