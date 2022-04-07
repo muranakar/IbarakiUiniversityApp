c7ba965484c2eb9523d512fcdd088c66f2990303
hirokaissh <hirokai.saito1014@gmail.com>
2022-04-07 23:12:03 +0900

    [delete] 一時不要な箇所を削除

--- IbarakiUiniversityApp/Controller/Document/DocumentsListViewController.swift
+++ IbarakiUiniversityApp/Controller/Document/DocumentsListViewController.swift
@@ -113,8 +113,4 @@ extension DocumentsListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 150
     }
-
-    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
-        view.tintColor = .clear
-    }
 }
