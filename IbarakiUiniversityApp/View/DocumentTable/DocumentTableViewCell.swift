b0309f370bcbaf36ccd6fa486e1b9f83e2e10e78
hirokaissh <hirokai.saito1014@gmail.com>
2022-04-11 19:57:16 +0900

    [add] documentCellにViewを配置してCellに隙間を確保する実装を行った

--- IbarakiUiniversityApp/View/DocumentTable/DocumentTableViewCell.swift
+++ IbarakiUiniversityApp/View/DocumentTable/DocumentTableViewCell.swift
@@ -11,14 +11,17 @@ class DocumentTableViewCell: UITableViewCell {
     @IBOutlet private(set) weak var documentNameLabel: UILabel!
     @IBOutlet private(set) weak var deadlineLabel: UILabel!
     @IBOutlet private(set) weak var documentCell: UIView!
+    @IBOutlet private(set) weak var documentContetView: UIView!
 
     override func awakeFromNib() {
         super.awakeFromNib()
 
-        documentCell.layer.cornerRadius = 20.0
+//        documentCell.layer.cornerRadius = 20.0
+        documentCell.backgroundColor = .clear
         documentCell.layer.shadowOffset = .init(width: 0.5, height: 0.5)
         documentCell.layer.shadowOpacity = 0.2
         documentCell.layer.shadowRadius = 20.0
+        documentContetView.layer.cornerRadius = 20.0
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
